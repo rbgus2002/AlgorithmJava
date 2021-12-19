0d422bee263a984bc625443140eb741b91839ddd
rbgus2002 <62997391+rbgus2002@users.noreply.github.com>
2021-12-19 12:59:46 +0900

    Test

--- BaekJoon/IF/P1330.java
+++ BaekJoon/IF/P1330.java
@@ -1,4 +1,28 @@
 package IF;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class P1330 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        System.out.println(versus(A, B));
+    }
+
+    public static String versus(int num1, int num2){
+        String answer;
+        if(num1 > num2)
+            answer = ">";
+        else if(num1 < num2)
+            answer = "<";
+        else
+            answer = "==";
+
+        return answer;
+    }
 }
