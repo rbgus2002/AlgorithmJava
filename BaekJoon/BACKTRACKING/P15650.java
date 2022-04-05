9d6b4950ac850910f04d047cc0807df3affba12a
rbgus2002 <62997391+rbgus2002@users.noreply.github.com>
2022-04-05 22:09:03 +0900

    .

--- BaekJoon/BACKTRACKING/P15650.java
+++ BaekJoon/BACKTRACKING/P15650.java
@@ -6,12 +6,40 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class P15650 {
+    private static boolean[] isVisit;
+    private static int[] num;
+    private static int N, M;
+    private static StringBuilder sb = new StringBuilder();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        isVisit = new boolean[N];
+        num = new int[M];
+        dfs(0);
+        System.out.println(sb);
+    }
 
+    private static void dfs(int depth){
+        if(M == depth){
+            for(int i = 0; i < num.length; i++){
+                sb.append(num[i]).append(' ');
+            }
+            sb.append("\n");
+            return;
+        }
 
+        for(int i = 0; i < isVisit.length; i++){
+            if(!isVisit[i]){
+                isVisit[i] = true;
+                num[depth] = i+1;
+                dfs(depth+1);
+                for(int j = i+1; j < isVisit.length; j++){
+                    isVisit[j] = false;
+                }
+            }
+        }
     }
 }
---
+++ BaekJoon/BACKTRACKING/P15652.java
@@ -0,0 +1,39 @@
+package BACKTRACKING;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P15652 {
+    private static StringBuilder sb = new StringBuilder();
+    private static boolean[] N;
+    private static int[] M;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = new boolean[Integer.parseInt(st.nextToken())];
+        M = new int[Integer.parseInt(st.nextToken())];
+        dfs(0);
+        System.out.println(sb);
+    }
+
+    private static void dfs(int depth) {
+        if (depth == M.length) {
+            for (int i = 0; i < M.length; i++) {
+                sb.append(M[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < N.length; i++) {
+            M[depth] = i + 1;
+            if(depth == 0)
+                dfs(depth + 1);
+            else if(depth > 0 && M[depth] >= M[depth-1])
+                dfs(depth + 1);
+        }
+    }
+}
